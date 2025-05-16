68773c5df3e8a43a5577798ba3768a9aac16406d
admin <admin@faas.com>
2025-05-16 17:50:03 +0530

    first commit

---
+++ func.py
@@ -0,0 +1,30 @@
+# from parliament import Context, event
+
+
+# @event
+# def main(context: Context):
+#     """
+#     Function template
+#     The context parameter contains the Flask request object and any
+#     CloudEvent received with the request.
+#     """
+
+#     # Add your business logic here
+
+#     # The return value here will be applied as the data attribute
+#     # of a CloudEvent returned to the function invoker
+#     return context.cloud_event.data
+
+
+from parliament import Context, event
+
+@event
+def main(context: Context):
+    otp_data = context.cloud_event.data
+
+    if isinstance(otp_data, dict) and "otp" in otp_data:
+        print(f"🔐 OTP received: {otp_data['otp']}")
+    else:
+        print("⚠️ No OTP found in message!")
+
+    return {"status": "OTP processed"}
