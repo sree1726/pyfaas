ab399a8730be7ce557cb3eaf22649b0be686a87c
sree1726 <sreekanth172652@gmail.com>
2025-05-20 19:09:33 +0530

    final.py

--- func.py
+++ func.py
@@ -1,19 +1,30 @@
+# from parliament import Context, event
 
-print("Script has been loaded. Waiting for events...")  #This runs at load time
+
+# @event
+# def main(context: Context):
+#     """
+#     Function template
+#     The context parameter contains the Flask request object and any
+#     CloudEvent received with the request.
+#     """
+
+#     # Add your business logic here
+
+#     # The return value here will be applied as the data attribute
+#     # of a CloudEvent returned to the function invoker
+#     return context.cloud_event.data
+
 
 from parliament import Context, event
 
 @event
 def main(context: Context):
-    print("Function 'main' started")  # This runs only on event trigger
-
     otp_data = context.cloud_event.data
-    print(f"Event data: {otp_data}")
 
     if isinstance(otp_data, dict) and "otp" in otp_data:
-        print(f"OTP received: {otp_data['otp']}")
+        print(f"🔐 OTP received: {otp_data['otp']}")
     else:
-        print("No OTP found in message!")
+        print("⚠️ No OTP found in message!")
 
-    print("Function completed")
     return {"status": "OTP processed"}
