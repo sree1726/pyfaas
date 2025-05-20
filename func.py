e480d219feba86505a97cd5b204d6c1115d0bb69
sree1726 <sreekanth172652@gmail.com>
2025-05-20 15:03:31 +0530

    Update func.py

--- func.py
+++ func.py
@@ -1,19 +1,19 @@
 
 print("Script has been loaded. Waiting for events...")  #This runs at load time
 
-from parliament import Context, event
+# from parliament import Context, event
 
-@event
-def main(context: Context):
-    print("Function 'main' started")  # This runs only on event trigger
+# @event
+# def main(context: Context):
+#     print("Function 'main' started")  # This runs only on event trigger
 
-    otp_data = context.cloud_event.data
-    print(f"Event data: {otp_data}")
+#     otp_data = context.cloud_event.data
+#     print(f"Event data: {otp_data}")
 
-    if isinstance(otp_data, dict) and "otp" in otp_data:
-        print(f"OTP received: {otp_data['otp']}")
-    else:
-        print("No OTP found in message!")
+#     if isinstance(otp_data, dict) and "otp" in otp_data:
+#         print(f"OTP received: {otp_data['otp']}")
+#     else:
+#         print("No OTP found in message!")
 
-    print("Function completed")
-    return {"status": "OTP processed"}
+#     print("Function completed")
+#     return {"status": "OTP processed"}
